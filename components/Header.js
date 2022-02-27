394f8df4c89906e08681ef6213e9224fb4d40f3f
maora96 <marinaoliveira.r07@gmail.com>
2022-02-27 17:37:29 -0300

    chore: project setup; feat: header styling.

---
+++ components/Header.js
@@ -0,0 +1,19 @@
+import styles from '../styles/Header.module.css'
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBarsStaggered } from '@fortawesome/free-solid-svg-icons';
+
+
+export default function Header() {
+
+    return (
+        <header className={styles.header}>
+            <h1 className={styles.header__h1}>Maora</h1>
+
+            <FontAwesomeIcon
+                icon={faBarsStaggered}
+                style={{ fontSize: 20, color: "#DAAF2A" }}
+            />
+        </header>
+    )
+}
